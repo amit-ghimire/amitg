cbaf3444f00e9380af5a32ff2e8fa4b62fa47251
amit-ghimire <amitghimiray07@outlook.com>
2026-04-08 19:29:36 +0545

    Minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             Projects
           </h2>
           <p className="mb-16 text-3xl font-bold tracking-tight">
-            Selected Work
+            Selected Works
           </p>
 
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
